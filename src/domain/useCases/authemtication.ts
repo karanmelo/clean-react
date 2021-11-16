46c417d40b7aef6696dccdc8caf5e4305c450f80
karanmelo <karan_melo@hotmail.com>
2021-11-15 21:56:40 -0300

    style: formatation

--- src/domain/useCases/authemtication.ts
+++ src/domain/useCases/authemtication.ts
@@ -6,5 +6,5 @@ type AuthenticationParams = {
 }
 
 export interface Authenticatino {
-  auth(params: AuthenticationParams): Promise<AccountModel>
+  auth: (params: AuthenticationParams) => Promise<AccountModel>
 }
