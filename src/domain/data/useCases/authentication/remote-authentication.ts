ca7e461f88e721a630d1e2ad6085670e78b9a7de
karanmelo <karan_melo@hotmail.com>
2021-12-02 22:54:00 -0300

    feat: ensure RemoteAuthentication throws InvalidCrendentialsError on 401

--- src/domain/data/useCases/authentication/remote-authentication.ts
+++ src/domain/data/useCases/authentication/remote-authentication.ts
@@ -1,5 +1,8 @@
 import { HttpPostClient } from 'src/domain/data/useCases/authentication/protocols/http/http-post-client'
+import { InvalidCredentialsError } from 'src/domain/errors/invalid-credentials-errors'
 import { AuthenticationParams } from 'src/domain/useCases/authemtication'
+
+import { HttpStatusCode } from './protocols/http/http-response'
 
 export class RemoteAuthentication {
   constructor (
@@ -8,6 +11,11 @@ export class RemoteAuthentication {
   ) {}
 
   async auth (params: AuthenticationParams): Promise<void> {
-    await this.httpPostClient.post({ url: this.url, body: params })
+    const httpResponse = await this.httpPostClient.post({ url: this.url, body: params })
+
+    switch (httpResponse.statusCode) {
+      case HttpStatusCode.noContent: return await Promise.resolve()
+      case HttpStatusCode.unauthorized: throw new InvalidCredentialsError()
+    }
   }
 }
--- src/domain/useCases/authemtication.ts
+++ src/domain/useCases/authemtication.ts
@@ -1,4 +1,4 @@
-import { AccountModel } from '../models/account-model'
+import { AccountModel } from 'src/domain/models'
 
 export type AuthenticationParams = {
   email: string
